6b2604a33f723f8e1b02a1b6b944eb4398541101
yayar <ya.yarochkin@yandex.ru>
2021-07-13 14:36:53 +0300

    multiple keyword to arr 1

--- top.py
+++ top.py
@@ -1,21 +1,15 @@
-out = open('prepared_data_for_Dost.txt', 'w', encoding='utf8')
+key = []
+key_sep = []
+out = open('prepared_data_for_Dost.py', 'w', encoding='utf8')
 res = []
-strings = []
-arrS = []
-s = ''
-a =     [(0,
-      '0.004*"молотов" + 0.004*"день" + 0.004*"немцы" + 0.004*"дома" + 0.003*"радио" + 0.003*"очень" + 0.003*"воиска" + 0.003*"воина" + 0.003*"наши" + 0.003*"города"'),
-     (1,
-      '0.005*"это" + 0.004*"день" + 0.004*"дома" + 0.004*"наши" + 0.003*"немцы" + 0.003*"воиска" + 0.003*"сад" + 0.003*"города" + 0.003*"ребята" + 0.003*"саду"'),
-     (2,
-      '0.004*"дома" + 0.004*"нам" + 0.003*"день" + 0.003*"наши" + 0.003*"это" + 0.003*"немцы" + 0.003*"взяли" + 0.003*"весь" + 0.003*"воиска" + 0.002*"свои"')]
-for topics in a:
-    strings.append(topics[1].split(' + '))
-for arr in strings:
-    for i in arr:
-        arrS.append(i.split('*'))
-
-for i in arrS:
-    res.append(i[1].replace('"', ''))
+a = open('topic_data1.txt', encoding='utf8').read().split('\'')
+for i in a:
+    if a.index(i) % 2 != 0:
+        key.append(i)
+for s in key:
+    key_sep.append(s.split(' + '))
+for e in key_sep:
+    for g in e:
+        res.append(g.split('*')[1].replace('"', ''))
 
 print(res, file=out)
